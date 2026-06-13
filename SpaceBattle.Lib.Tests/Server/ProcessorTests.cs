491a78d3bc3d55936e8f781c70221f3f25c654d2
Yaroslav <burov.yarv@gmail.com>
2026-06-13 23:44:04 +0300

    feat: Цикл обработки команд с потокобезопасной очередью

---
+++ SpaceBattle.Lib.Tests/Server/ProcessorTests.cs
@@ -0,0 +1,29 @@
+using System.Collections.Concurrent;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class ProcessorTests
+{
+    // Поток в цикле берёт команды из очереди и выполняет их
+    [Fact]
+    public void Processor_ExecutesCommandsFromQueue()
+    {
+        var context = new Dictionary<string, object>();
+        new InitProcessorContextCommand(context).Execute();
+
+        var queue = (BlockingCollection<ICommand>)context["queue"];
+
+        var cmd = new Mock<ICommand>();
+        queue.Add(cmd.Object);
+        // команда-стоп: останавливает цикл, чтобы поток завершился и тест не повис
+        queue.Add(new ActionCommand(() => context["canContinue"] = false));
+
+        var processor = new Processor(new Processable(context));
+
+        // поток успел завершиться в отведённое время
+        Assert.True(processor.Wait(5000));
+        // команда из очереди была выполнена ровно один раз
+        cmd.Verify(c => c.Execute(), Times.Once());
+    }
+}
